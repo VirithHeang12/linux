dade4486bc0907c0569383291e3963e35bd29dda
LaFii <sophannarith38@gmail.com>
2025-03-09 19:56:01 +0700

    Enhance validation messages for login and student creation requests, and improve login form layout

--- app/Http/Requests/StudentStoreRequest.php
+++ app/Http/Requests/StudentStoreRequest.php
@@ -34,4 +34,25 @@ class StudentStoreRequest extends FormRequest
             'phone'         => ['required', 'string', 'regex:/^(\+?\d{1,3})? ?\d{8,15}$/']
         ];
     }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'student_id.required'    => 'Student ID is required.',
+            'student_id.string'      => 'Student ID must be a string.',
+            'student_id.max'         => 'Student ID must not be greater than 255 characters.',
+            'student_id.unique'      => 'Student ID must be unique. AH bek',
+            'first_name.required'    => 'First name is required.',
+            'first_name.string'      => 'First name must be a string.',
+            'first_name.max'         => 'First name must not be greater than 255 characters.',
+            'last_name.required'     => 'Last name is required.',
+            'last_name.string'       => 'Last name must be a string.',
+            'last_name.max'          => 'Last name must not be greater than 255 characters.',
+        ];
+    }
 }
