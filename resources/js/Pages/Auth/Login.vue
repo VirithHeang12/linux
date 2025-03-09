a09c530d7771bfb921900198f83cbbe8e269333c
LaFii <sophannarith38@gmail.com>
2025-03-09 15:45:26 +0700

    Enhance login page layout with logo and improve form structure

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,38 +1,43 @@
 <template>
-    <vee-form :validation-schema="schema" @submit.prevent="submitForm" v-slot="{ meta, setErrors }">
-        <vee-field name="email" v-slot="{ field, errors }">
-            <v-text-field v-bind="field" :error-messages="errors" v-model="form.email" label="Email"
-                variant="outlined"></v-text-field>
-        </vee-field>
-        <vee-field name="password" v-slot="{ field, errors }">
-            <v-text-field v-bind="field" :error-messages="errors" v-model="form.password" label="Password"
-                variant="outlined"></v-text-field>
-        </vee-field>
-        <v-btn color="primary" class="mt-4 d-inline-flex justify-content-start "
-            :disabled="!meta.valid || form.processing" @click.prevent="submitForm(setErrors)">
-            <span v-if="form.processing" class="spinner-border spinner-border-sm me-2" role="status"
-                aria-hidden="true"></span>
-            Submit
-        </v-btn>
-    </vee-form>
+    <div>
+        <v-container>
+            <img src="public/assets/logo.png" alt="">
+        </v-container>
+        <vee-form :validation-schema="schema" @submit.prevent="submitForm" v-slot="{ meta, setErrors }">
+            <vee-field name="email" v-slot="{ field, errors }">
+                <v-text-field v-bind="field" :error-messages="errors" v-model="form.email" label="Email"
+                    variant="outlined"></v-text-field>
+            </vee-field>
+            <vee-field name="password" v-slot="{ field, errors }">
+                <v-text-field v-bind="field" :error-messages="errors" v-model="form.password" label="Password"
+                    variant="outlined"></v-text-field>
+            </vee-field>
+            <v-btn color="primary" class="mt-4 d-inline-flex justify-content-start "
+                :disabled="!meta.valid || form.processing" @click.prevent="submitForm(setErrors)">
+                <span v-if="form.processing" class="spinner-border spinner-border-sm me-2" role="status"
+                    aria-hidden="true"></span>
+                Submit
+            </v-btn>
+        </vee-form>
+    </div>
 </template>
 
 <script setup>
-    import { useForm } from '@inertiajs/vue3';
-    import * as yup from 'yup';
-    import { route } from 'ziggy-js';
+import { useForm } from '@inertiajs/vue3';
+import * as yup from 'yup';
+import { route } from 'ziggy-js';
 
-    const schema = yup.object().shape({
-        email: yup.string().required().email(),
-        password: yup.string().required().min(6),
-    })
+const schema = yup.object().shape({
+    email: yup.string().required().email(),
+    password: yup.string().required().min(6),
+})
 
-    const form = useForm({
-        email: '',
-        password: '',
-    });
+const form = useForm({
+    email: '',
+    password: '',
+});
 
-    const submitForm = () => {
-        form.post(route('login.store'));
-    }
+const submitForm = () => {
+    form.post(route('login.store'));
+}
 </script>
